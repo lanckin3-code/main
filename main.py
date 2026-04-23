8ccc1a85924a7f4b9924f994a91d897211b00c02
lanckin3-code <lanckin3@gmail.com>
2026-04-23 23:04:56 +0700

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ BOT_TOKEN = "8771131700:AAEauoHEPmXfU0nxR5NaAl1gE3MXJxaIdWQ"
 PHOTO_START = "https://kappa.lol/v03Ziu"           # Фото для команды /start
 PHOTO_BUY_PASS = "https://kappa.lol/SZivBN"        # Фото для покупки проходки
 PHOTO_SUBMIT_REQUEST = "https://kappa.lol/CqVnrk"  # Фото для заявки
-PHOTO_BUILD = "https://kappa.lol/ваша_ссылка"      # 🆕 Фото для сборки (ЗАМЕНИТЕ!)
+PHOTO_BUILD = "https://kappa.lol/v03Ziu"      # 🆕 Фото для сборки (ЗАМЕНИТЕ!)
 
 # Текст для сообщений
 TEXT_BUY_PASS = "🔴 https://www.donationalerts.com/r/slimehook 🔴"
